a27750f65c9740f9d1fec82469eca8a8dd94584a
dlugi57 <dlugoszpeter@gmail.com>
2019-04-09 22:02:00 +0200

    new conditions to password only with js

--- Projet4/alaska/public/js/scripts.js
+++ Projet4/alaska/public/js/scripts.js
@@ -49,5 +49,35 @@ $('#registerBtn').click(function() {
   }
 
 
+  var passwordValue = $('#password').val();
+
+  if (passwordValue.length < 8) {
+    $('#confirmPass').addClass('is-invalid');
+    $('.invalid-feedback').text("Le mot de passe doit être de 8 caractères minimum");
+    return false;
+  }
+
+  re = /[0-9]/;
+  if (!re.test(passwordValue)) {
+    $('#confirmPass').addClass('is-invalid');
+    $('.invalid-feedback').text("Le mot de passe doit contenir au moins 1 chiffre. ");
+    return false;
+  }
+  re = /[a-z]/;
+  if (!re.test(passwordValue)) {
+    $('#confirmPass').addClass('is-invalid');
+    $('.invalid-feedback').text("Le mot de passe doit contenir au moins 1 caractère en minuscule");
+    return false;
+  }
+  re = /[A-Z]/;
+  if (!re.test(passwordValue)) {
+
+    $('#confirmPass').addClass('is-invalid');
+    $('.invalid-feedback').text("Le mot de passe doit contenir au moins 1 caractère en majuscule");
+    return false;
+  }
+
+
+
 
 })

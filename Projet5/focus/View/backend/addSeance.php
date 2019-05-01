3abcd96e86095612ca3e083863354629037a16fc
dlugi57 <dlugoszpeter@gmail.com>
2019-05-01 14:46:42 +0200

    clean of add seance html

--- Projet5/focus/View/backend/addSeance.php
+++ Projet5/focus/View/backend/addSeance.php
@@ -9,7 +9,6 @@ ob_start();
     <small>Control panel</small>
   </h1>
 </section>
-
 <!-- Main content -->
 <section class="content">
   <!-- general form elements disabled -->
@@ -19,169 +18,149 @@ ob_start();
     </div>
     <!-- /.box-header -->
     <form role="form" method="post" action="index.php?action=addSeance">
-    <div class="box-body">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="form-group">
-            <label>Nom </label>
-            <select name="clients_id" class="form-control select2">
-              <?php
-              if (isset($client['name'])):
-                ?>
-                <option value="<?= $client['id_client'] ?>" selected="selected"><?= $client['name'] ?></option>
-                <?php
-              else:
-                ?>
-                <option value="" selected="selected"></option>
-                <?php
-              endif;
-              while ($data = $clientsList->fetch())
-              {
-                ?>
-                <option value="<?= $data['id_client'] ?>"><?= $data['name'] ?></option>
-                <?php
-              }
-              ?>
-            </select>
-          </div>
-          <!-- select -->
-          <div class="form-group">
-            <label>Seance type</label>
-            <select name="type" class="form-control">
-              <?php
-              while ($data = $newSeancePage->fetch()) {
-
-                ?>
-                <option value="<?= $data['id_type_seance'] ?>"><?= $data['nom_type'] ?></option>
-                <?php
-
-              }
-               ?>
-            </select>
-        </div>
-
-        <!-- Date -->
-        <div class="form-group">
-          <label>Date seance:</label>
-
-          <div class="input-group date">
-            <div class="input-group-addon">
-              <i class="fa fa-calendar"></i>
-            </div>
-            <input name="seance_date" type="text" class="form-control pull-right" id="datepickerSeance">
-          </div>
-          <!-- /.input group -->
-        </div>
-        <!-- /.form group -->
-
-        <!-- time Picker -->
-        <div class="bootstrap-timepicker">
-          <div class="form-group">
-            <label>Heure:</label>
-            <div class="input-group">
-              <div class="input-group-addon">
-                <i class="fa fa-clock-o"></i>
-              </div>
-              <input name="time_seance" type="text" class="form-control timepicker">
-            </div>
-            <!-- /.input group -->
-          </div>
-          <!-- /.form group -->
-        </div>
-
-
-        <!-- Prix -->
-        <div class="form-group">
-          <label>Prix</label>
-
-          <div class="input-group date">
-            <div class="input-group-addon">
-              <i class="fa fa-eur"></i>
-            </div>
-            <input name="prise" type="number" class="form-control">
-          </div>
-          <!-- /.input group -->
-        </div>
-        <!-- /.form group -->
-
-        <!-- Depenses -->
-        <div class="form-group">
-          <label>Depenses</label>
-
-          <div class="input-group date">
-            <div class="input-group-addon">
-              <i class="fa fa-money"></i>
-            </div>
-            <input name="depenses" type="number" class="form-control">
-          </div>
-          <!-- /.input group -->
-        </div>
-        <!-- /.form group -->
-
-
-
-
-
-
-
-
-
-          </div>
-          <!-- /.col -->
+      <div class="box-body">
+        <div class="row">
+          <!-- col left -->
           <div class="col-md-6">
+            <div class="form-group">
+              <label>Nom </label>
+              <select name="clients_id" class="form-control select2">
+                <?php
+                if (isset($client['name'])):
+                  ?>
+                  <option value="<?= $client['id_client'] ?>" selected="selected"><?= $client['name'] ?></option>
+                  <?php
+                else:
+                  ?>
+                  <option value="" selected="selected"></option>
+                  <?php
+                endif;
+                while ($data = $clientsList->fetch())
+                {
+                  ?>
+                  <option value="<?= $data['id_client'] ?>"><?= $data['name'] ?></option>
+                  <?php
+                }
+                ?>
+              </select>
+            </div>
+            <!-- select -->
+            <div class="form-group">
+              <label>Seance type</label>
+              <select name="type" class="form-control">
+                <?php
+                while ($data = $newSeancePage->fetch())
+                {
+                  ?>
+                  <option value="<?= $data['id_type_seance'] ?>"><?= $data['nom_type'] ?></option>
+                  <?php
+                }
+                 ?>
+              </select>
+            </div>
 
+            <!-- Date -->
+            <div class="form-group">
+              <label>Date seance:</label>
+              <div class="input-group date">
+                <div class="input-group-addon">
+                  <i class="fa fa-calendar"></i>
+                </div>
+                <input name="seance_date" type="text" class="form-control pull-right" id="datepickerSeance">
+              </div>
+              <!-- /.input group -->
+            </div>
+            <!-- /.form group -->
+
+            <!-- time Picker -->
+            <div class="bootstrap-timepicker">
+              <div class="form-group">
+                <label>Heure:</label>
+                <div class="input-group">
+                  <div class="input-group-addon">
+                    <i class="fa fa-clock-o"></i>
+                  </div>
+                  <input name="time_seance" type="text" class="form-control timepicker">
+                </div>
+                <!-- /.input group -->
+              </div>
+              <!-- /.form group -->
+            </div>
+
+            <!-- Prix -->
+            <div class="form-group">
+              <label>Prix</label>
+              <div class="input-group date">
+                <div class="input-group-addon">
+                  <i class="fa fa-eur"></i>
+                </div>
+                <input name="prise" type="number" class="form-control">
+              </div>
+              <!-- /.input group -->
+            </div>
+            <!-- /.form group -->
+
+            <!-- Depenses -->
+            <div class="form-group">
+              <label>Depenses</label>
+              <div class="input-group date">
+                <div class="input-group-addon">
+                  <i class="fa fa-money"></i>
+                </div>
+                <input name="depenses" type="number" class="form-control">
+              </div>
+              <!-- /.input group -->
+            </div>
+            <!-- /.form group -->
+          </div>
+          <!-- /.col left -->
+
+          <!-- col roght -->
+          <div class="col-md-6">
+            <!-- model -->
             <div class="form-group">
               <label>Model</label>
               <input name="model" type="text" class="form-control">
             </div>
-
+            <!-- adresse -->
             <div class="form-group">
               <label>Adresse</label>
               <input name="adresse_seance" type="text" class="form-control">
             </div>
+            <!-- city -->
             <div class="form-group">
               <label>Ville</label>
               <input name="city_seance" type="text" class="form-control">
             </div>
+            <!-- distance -->
             <div class="form-group">
               <label>Distance KM</label>
               <input name="km" class="form-control" type="number">
             </div>
-
-            <!-- textarea -->
+            <!-- description -->
             <div class="form-group">
               <label>Description</label>
               <textarea name="description_seance" class="form-control" rows="5"></textarea>
             </div>
-
-
-            </div>
-
-
-
-
+          </div>
+          <!--/.col right-->
         </div>
-        <!--/.col-->
-
-
-
-
-
-      <!--/.row-->
-      <div class="box-footer">
-        <?php
-        //if we can return to the last page show button
-        if (isset($_SERVER['HTTP_REFERER'])) {
-        ?>
-        <a class="btn btn-default" href="<?= $_SERVER['HTTP_REFERER'] ?>">Retour</a>
-        <?php
-        }
-        ?>
-        <button type="submit" class="btn btn-info pull-right">Ajouter</button>
+        <!--/.row-->
+        <!--footer-->
+        <div class="box-footer">
+          <?php
+          //if we can return to the last page show button
+          if (isset($_SERVER['HTTP_REFERER'])):
+            ?>
+            <a class="btn btn-default" href="<?= $_SERVER['HTTP_REFERER'] ?>">Retour</a>
+            <?php
+          endif;
+          ?>
+          <button type="submit" class="btn btn-info pull-right">Ajouter</button>
+        </div>
+        <!-- /.box-footer -->
       </div>
-      <!-- /.box-footer -->
-    </div>
-    <!-- /.box-body -->
+      <!-- /.box-body -->
     </form>
   </div>
 </section>

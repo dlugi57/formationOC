92269c36b9bd2bbb160b30779524f15fdbb1e6af
dlugi57 <dlugoszpeter@gmail.com>
2019-05-02 12:07:00 +0200

    SECURITY exceptions into focus and focus back class

--- Projet5/focus/Controller/Focus.php
+++ Projet5/focus/Controller/Focus.php
@@ -1,11 +1,5 @@
 <?php
-
 namespace Controller;
-
-//require_once "Model/ClientManager.php";
-//require_once "Model/SeanceManager.php";
-//require_once "Model/CommandManager.php";
-//require_once "Model/TaxesManager.php";
 
 use Model\SeanceManager;
 use Model\ClientManager;
@@ -15,18 +9,22 @@ use Exception;
 
 class Focus
 {
-    //CLIENTS
+    //----------------
+    //- CLIENTS
+    //----------------
     public function listClients()
     {
         $clientsManager = new ClientManager();
         $clients = $clientsManager->getClients();
-        if ($clients === false)
-        {
-            throw new Exception('Impossible d\'afficher le contenue !');
-        }else
-        {
+        if ($clients === false):
+
+            throw new Exception('Impossible d\'afficher le contenue de clients list !');
+
+        else:
+
             require('View/frontend/clientsList.php');
-        }
+
+        endif;
     }
 
     function client()
@@ -38,128 +36,139 @@ class Focus
         $commandsManager = new CommandManager();
         $command = $commandsManager->getClientCommand($_GET['id']);
 
-        if ($client === false)
-        {
-            throw new Exception('Impossible d\'afficher le chapitre !');
-        }
-        else
-        {
+        if ($client === false || $seance === false || $command === false):
+
+            throw new Exception('Impossible d\'afficher le client contenue !');
+
+        else:
+
             require('View/frontend/client.php');
-        }
+
+        endif;
     }
 
-
-
-
-
-    //SEANCES
+    //----------------
+    //- SEANCES
+    //----------------
     public function listSeances()
     {
         $seancesManager = new SeanceManager();
         $seances = $seancesManager->getSeances();
-        //$seancesMonthly = $seancesManager->monthSeances();
         $cashTypeSeance = $seancesManager->typeCashSession();
         $cashSummarySeance = $seancesManager->totals();
         $monthSeances = $seancesManager->monthSeances();
-        $resultsMonthSeance = array();
-        $resultsSeancesCash = array();
-        $resultsSeancesDepenses = array();
-        while ($data = $monthSeances->fetch())
-        {
-            $monthNum  = htmlspecialchars($data['month']);
-            $monthName = date('F', mktime(0, 0, 0, $monthNum, 10));
-            $depensesSeance = htmlspecialchars($data['drove']) * 0.15 +htmlspecialchars($data['paied']);
-            $depensesSeanceSummary = htmlspecialchars($data['cash']) - intval($depensesSeance);
-            array_push($resultsMonthSeance, $monthName);
-            array_push($resultsSeancesCash, intval(htmlspecialchars($data['cash'])));
-            array_push($resultsSeancesDepenses, $depensesSeanceSummary);
-        }
-        if ($seances === false)
-        {
-            throw new Exception('Impossible d\'afficher le contenue !');
-        }else
-        {
+
+
+        if ($seances === false || $cashTypeSeance === false || $cashSummarySeance === false || $monthSeances === false):
+
+            throw new Exception('Impossible d\'afficher le listSeances contenue !');
+
+        else:
+
+            $resultsMonthSeance = array();
+            $resultsSeancesCash = array();
+            $resultsSeancesDepenses = array();
+            while ($data = $monthSeances->fetch())
+            {
+
+                $monthNum  = htmlspecialchars($data['month']);
+                $monthName = date('F', mktime(0, 0, 0, $monthNum, 10));
+                $depensesSeance = htmlspecialchars($data['drove']) * 0.15 +htmlspecialchars($data['paied']);
+                $depensesSeanceSummary = htmlspecialchars($data['cash']) - intval($depensesSeance);
+                array_push($resultsMonthSeance, $monthName);
+                array_push($resultsSeancesCash, intval(htmlspecialchars($data['cash'])));
+                array_push($resultsSeancesDepenses, $depensesSeanceSummary);
+            }
+
             require('View/frontend/seancesList.php');
-        }
+
+        endif;
     }
 
     public function seance()
     {
-      $seancesManager = new SeanceManager();
-      $seance = $seancesManager->getSeance($_GET['id']);
-      if ($seance === false)
-      {
-          throw new Exception('Impossible d\'afficher le contenue !');
-      }else
-      {
-          require('View/frontend/seance.php');
-      }
+        $seancesManager = new SeanceManager();
+        $seance = $seancesManager->getSeance($_GET['id']);
+        if ($seance === false):
+
+            throw new Exception('Impossible d\'afficher le seance contenue !');
+        else:
+
+            require('View/frontend/seance.php');
+
+        endif;
     }
 
-    /*public function recapSeanceMonth(){
-      $seancesManager = new SeanceManager();
-
-      return $resultsMonthSeance;
-    }*/
-
-
+    //----------------
+    //- COMMANDS
+    //----------------
     public function listCommands()
     {
-      $commandsManager = new CommandManager();
-      $commands = $commandsManager->getCommands();
-      $commandsTotal = $commandsManager->totalsCmd();
-      if ($commands === false)
-      {
-          throw new Exception('Impossible d\'afficher le contenue !');
-      }else
-      {
-          require('View/frontend/commandsList.php');
-      }
+        $commandsManager = new CommandManager();
+        $commands = $commandsManager->getCommands();
+        $commandsTotal = $commandsManager->totalsCmd();
 
+        if ($commands === false || $commandsTotal === false):
+
+            throw new Exception('Impossible d\'afficher le listCommands contenue !');
+
+        else:
+
+            require('View/frontend/commandsList.php');
+
+        endif;
     }
 
     public function command()
     {
-      $commandsManager = new CommandManager();
-      $command = $commandsManager->getCommand($_GET['id']);
+        $commandsManager = new CommandManager();
+        $command = $commandsManager->getCommand($_GET['id']);
 
-      if ($command === false)
-      {
-          throw new Exception('Impossible d\'afficher le contenue !');
-      }else
-      {
-          require('View/frontend/command.php');
-      }
+        if ($command === false):
 
+            throw new Exception('Impossible d\'afficher le command contenue !');
+
+        else:
+
+            require('View/frontend/command.php');
+
+        endif;
     }
 
+    //----------------
+    //- TAXES
+    //----------------
     public function listTaxes()
     {
-      $taxesManager = new TaxesManager();
-      $taxes = $taxesManager->getTaxes();
-      $totalTaxes = $taxesManager->totalsTax();
-      if ($taxes === false)
-      {
-          throw new Exception('Impossible d\'afficher le contenue !');
-      }else
-      {
-          require('View/frontend/taxesList.php');
-      }
+        $taxesManager = new TaxesManager();
+        $taxes = $taxesManager->getTaxes();
+        $totalTaxes = $taxesManager->totalsTax();
+        if ($taxes === false || $totalTaxes === false):
+
+            throw new Exception('Impossible d\'afficher le listTaxes contenue !');
+
+        else:
+
+            require('View/frontend/taxesList.php');
+
+        endif;
 
     }
 
     public function taxe()
     {
-      $taxesManager = new TaxesManager();
-      $taxe = $taxesManager->getTaxe($_GET['id']);
+        $taxesManager = new TaxesManager();
+        $taxe = $taxesManager->getTaxe($_GET['id']);
 
-      if ($taxe === false)
-      {
-          throw new Exception('Impossible d\'afficher le contenue !');
-      }else
-      {
-          require('View/frontend/taxe.php');
-      }
+        if ($taxe === false):
+
+            throw new Exception('Impossible d\'afficher le contenue !');
+
+        else:
+
+            require('View/frontend/taxe.php');
+
+        endif;
     }
 
     /*public function instagram(){
@@ -167,7 +176,6 @@ class Focus
       $api_response = file_get_contents($url);
       $record = json_decode($api_response);
       return $record;
-
     }*/
 
 }
--- Projet5/focus/Controller/FocusBack.php
+++ Projet5/focus/Controller/FocusBack.php
@@ -1,11 +1,5 @@
 <?php
-
 namespace Controller;
-
-//require_once "Model/ClientManager.php";
-//require_once "Model/SeanceManager.php";
-//require_once "Model/CommandManager.php";
-//require_once "Model/TaxesManager.php";
 
 use Model\SeanceManager;
 use Model\ClientManager;
@@ -13,32 +7,33 @@ use Model\CommandManager;
 use Model\TaxesManager;
 use Exception;
 
-/**
- *
- */
 class FocusBack
 {
 
-  //CLIENT
+  //----------------
+  //- CLIENTS
+  //----------------
   public function addClientPage()
   {
       $clientsManager = new ClientManager();
       $newClientPage = $clientsManager->contactByList();
+
       if ($newClientPage === false):
 
-          throw new Exception('Impossible d\'afficher le client !');
+          throw new Exception('Impossible d\'afficher le client ajout page!');
 
       else:
 
           require('View/backend/addClient.php');
-      endif;
 
+      endif;
   }
 
   public function addClient()
   {
       $clientsManager = new ClientManager();
       $newClient = $clientsManager->newClient($_POST['name'], $_POST['tel'], $_POST['email'], $_POST['adress'], $_POST['city'], $_POST['post_code'], $_POST['contact_by'], $_POST['description']);
+
       if ($newClient === false):
 
           throw new Exception('Impossible d\'ajouter le client !');
@@ -54,25 +49,44 @@ class FocusBack
   public function modifyClientPage()
   {
       $clientsManager = new ClientManager();
-      $modifyClient = $clientsManager->getClient($_GET['id']);
+
+      if (isset($_GET['id']) && $_GET['id'] > 0):
+
+          $modifyClient = $clientsManager->getClient($_GET['id']);
+
+      else:
+
+          throw new Exception("Aucun identifiant de client envoyé");
+
+      endif;
+
       $modifyClientContact = $clientsManager->contactByList();
 
+      if ($modifyClient === false || $modifyClientContact === false):
 
-      if ($modifyClient === false):
-
-          throw new Exception('Impossible d\'afficher le client !');
+          throw new Exception('Impossible d\'afficher le client modifier page!');
 
       else:
 
           require('View/backend/modifyClient.php');
 
       endif;
-
   }
+
   public function modifyClient()
   {
       $clientsManager = new ClientManager();
-      $updateClient = $clientsManager->updateClient($_GET['id'],$_POST['name'], $_POST['tel'], $_POST['email'], $_POST['adress'], $_POST['city'], $_POST['post_code'], $_POST['contact_by'], $_POST['description']);
+
+      if (isset($_GET['id']) && $_GET['id'] > 0):
+
+          $updateClient = $clientsManager->updateClient($_GET['id'],$_POST['name'], $_POST['tel'], $_POST['email'], $_POST['adress'], $_POST['city'], $_POST['post_code'], $_POST['contact_by'], $_POST['description']);
+
+      else:
+
+          throw new Exception("Aucun identifiant de client envoyé");
+
+      endif;
+
       if ($updateClient === false):
 
           throw new Exception('Impossible de modifier le client !');
@@ -82,13 +96,23 @@ class FocusBack
           header('Location: index.php?action=client&id='.$_GET['id'].'');
 
       endif;
-
   }
 
   public function removeClient()
   {
     $clientsManager = new ClientManager();
-    $deleteClient = $clientsManager->deleteClient($_GET['id']);
+
+
+    if (isset($_GET['id']) && $_GET['id'] > 0):
+
+        $deleteClient = $clientsManager->deleteClient($_GET['id']);
+
+    else:
+
+        throw new Exception("Aucun identifiant de client envoyé");
+
+    endif;
+
     if ($deleteClient === false):
 
         throw new Exception('Impossible de supprime client !');
@@ -100,27 +124,31 @@ class FocusBack
     endif;
   }
 
-  //SEANCE
+  //----------------
+  //- SEANCE
+  //----------------
   public function addSeancePage()
   {
       $seancesManager = new SeanceManager();
       $newSeancePage = $seancesManager->typeSeanceList();
       $clientsManager = new ClientManager();
+
       if (isset($_GET['id']) && $_GET['id'] > 0):
 
-        $client = $clientsManager->getClient($_GET['id']);
+          $client = $clientsManager->getClient($_GET['id']);
 
       endif;
 
       $clientsList = $clientsManager->getClients();
 
-      if ($newSeancePage === false):
+      if ($newSeancePage === false || $clientsList === false):
 
-          throw new Exception('Impossible d\'afficher le client !');
+          throw new Exception('Impossible d\'afficher le seance page !');
 
       else:
 
           require('View/backend/addSeance.php');
+
       endif;
 
   }
@@ -129,6 +157,7 @@ class FocusBack
   {
       $seancesManager = new SeanceManager();
       $newSeance = $seancesManager->newSeance($_POST['clients_id'], $_POST['type'], $_POST['seance_date'], $_POST['time_seance'], $_POST['prise'], $_POST['depenses'], $_POST['model'], $_POST['adresse_seance'], $_POST['city_seance'], $_POST['km'], $_POST['description_seance']);
+
       if ($newSeance === false):
 
           throw new Exception('Impossible d\'ajouter le seance !');
@@ -140,20 +169,27 @@ class FocusBack
       endif;
   }
 
-
-
   public function modifySeancePage()
   {
       $seancesManager = new SeanceManager();
-      $modifySeance = $seancesManager->getSeance($_GET['id']);
+
+      if (isset($_GET['id']) && $_GET['id'] > 0):
+
+          $modifySeance = $seancesManager->getSeance($_GET['id']);
+
+      else:
+
+          throw new Exception("Aucun identifiant de seance envoyé");
+
+      endif;
+
       $modifySeanceType = $seancesManager->typeSeanceList();
       $clientsManager = new ClientManager();
       $modifySeanceClients = $clientsManager->getClients();
 
+      if ($modifySeance === false || $modifySeanceType === false || $modifySeanceClients === false):
 
-      if ($modifySeance === false):
-
-          throw new Exception('Impossible d\'afficher le seance !');
+          throw new Exception('Impossible d\'afficher le seance modifier page!');
 
       else:
 
@@ -165,7 +201,18 @@ class FocusBack
   public function modifySeance()
   {
       $seancesManager = new SeanceManager();
-      $updateSeance = $seancesManager->updateSeance($_GET['id'],$_POST['clients_id'], $_POST['type'], $_POST['seance_date'], $_POST['time_seance'], $_POST['prise'], $_POST['depenses'], $_POST['model'], $_POST['adresse_seance'], $_POST['city_seance'], $_POST['km'], $_POST['description_seance']);
+
+
+      if (isset($_GET['id']) && $_GET['id'] > 0):
+
+          $updateSeance = $seancesManager->updateSeance($_GET['id'],$_POST['clients_id'], $_POST['type'], $_POST['seance_date'], $_POST['time_seance'], $_POST['prise'], $_POST['depenses'], $_POST['model'], $_POST['adresse_seance'], $_POST['city_seance'], $_POST['km'], $_POST['description_seance']);
+
+      else:
+
+          throw new Exception("Aucun identifiant de seance envoyé");
+
+      endif;
+
       if ($updateSeance === false):
 
           throw new Exception('Impossible de modifier le seance !');
@@ -180,20 +227,32 @@ class FocusBack
 
   public function removeSeance()
   {
-    $seancesManager = new SeanceManager();
-    $deleteSeance = $seancesManager->deleteSeance($_GET['id']);
-    if ($deleteSeance === false):
+      $seancesManager = new SeanceManager();
 
-        throw new Exception('Impossible de supprimer le seance !');
+      if (isset($_GET['id']) && $_GET['id'] > 0):
 
-    else:
+          $deleteSeance = $seancesManager->deleteSeance($_GET['id']);
 
-        header('Location: index.php?action=dashboard');
+      else:
 
-    endif;
+          throw new Exception("Aucun identifiant de seance envoyé");
+
+      endif;
+
+      if ($deleteSeance === false):
+
+          throw new Exception('Impossible de supprimer le seance !');
+
+      else:
+
+          header('Location: index.php?action=dashboard');
+
+      endif;
   }
 
-  //COMMAND
+  //----------------
+  //- COMMAND
+  //----------------
   public function addCommandPage()
   {
       $commandsManager = new CommandManager();
@@ -208,13 +267,14 @@ class FocusBack
 
       $clientsList = $clientsManager->getClients();
 
-      if ($newCommandPage === false):
+      if ($newCommandPage === false || $client === false || $clientsList === false):
 
-          throw new Exception('Impossible d\'afficher le client !');
+          throw new Exception('Impossible d\'afficher le commande page !');
 
       else:
 
           require('View/backend/addCommand.php');
+
       endif;
   }
 
@@ -222,6 +282,7 @@ class FocusBack
   {
       $commandsManager = new CommandManager();
       $newCommand = $commandsManager->newCommand($_POST['client_id_cmd'], $_POST['type_command'], $_POST['description_command'], $_POST['prise_command'], $_POST['cost_command']);
+
       if ($newCommand === false):
 
           throw new Exception('Impossible d\'ajouter la commande !');
@@ -235,29 +296,48 @@ class FocusBack
 
   public function modifyCommandPage()
   {
-    $commandsManager = new CommandManager();
-    $modifyCommand = $commandsManager->getCommand($_GET['id']);
+      $commandsManager = new CommandManager();
 
-    $modifyCommandType = $commandsManager->typeCommandList();
-    $clientsManager = new ClientManager();
-    $modifyCommandClients = $clientsManager->getClients();
+      if (isset($_GET['id']) && $_GET['id'] > 0):
+
+          $modifyCommand = $commandsManager->getCommand($_GET['id']);
+
+      else:
+
+          throw new Exception("Aucun identifiant de commande envoyé");
+
+      endif;
+
+      $modifyCommandType = $commandsManager->typeCommandList();
+      $clientsManager = new ClientManager();
+      $modifyCommandClients = $clientsManager->getClients();
 
 
-    if ($modifyCommand === false):
+      if ($modifyCommand === false || $modifyCommandType === false || $modifyCommandClients === false):
 
-        throw new Exception('Impossible d\'afficher la commande !');
+          throw new Exception('Impossible d\'afficher la commande modifier page !');
 
-    else:
+      else:
 
-        require('View/backend/modifyCommand.php');
+          require('View/backend/modifyCommand.php');
 
-    endif;
+      endif;
   }
 
   public function modifyCommand()
   {
     $commandsManager = new CommandManager();
-    $updateCommand = $commandsManager->updateCommand($_GET['id'],$_POST['client_id_cmd'], $_POST['type_command'], $_POST['description_command'], $_POST['prise_command'], $_POST['cost_command']);
+
+    if (isset($_GET['id']) && $_GET['id'] > 0):
+
+        $updateCommand = $commandsManager->updateCommand($_GET['id'],$_POST['client_id_cmd'], $_POST['type_command'], $_POST['description_command'], $_POST['prise_command'], $_POST['cost_command']);
+
+    else:
+
+        throw new Exception("Aucun identifiant de commande envoyé");
+
+    endif;
+
     if ($updateCommand === false):
 
         throw new Exception('Impossible de modifier le commande !');
@@ -272,7 +352,17 @@ class FocusBack
   public function removeCommand()
   {
     $commandsManager = new CommandManager();
-    $deleteCommand = $commandsManager->deleteCommand($_GET['id']);
+
+    if (isset($_GET['id']) && $_GET['id'] > 0):
+
+        $deleteCommand = $commandsManager->deleteCommand($_GET['id']);
+
+    else:
+
+        throw new Exception("Aucun identifiant de commande envoyé");
+
+    endif;
+
     if ($deleteCommand === false):
 
         throw new Exception('Impossible de supprimer la commande !');
@@ -284,14 +374,14 @@ class FocusBack
     endif;
   }
 
-  //TAXES
-
-
-
+  //----------------
+  //- TAXES
+  //----------------
   public function addTaxes()
   {
     $taxesManager = new TaxesManager();
     $newTaxe = $taxesManager->newTaxes($_POST['tax_date'], $_POST['tax_declare'], $_POST['tax_paid'], $_POST['tax_description']);
+
     if ($newTaxe === false):
 
         throw new Exception('Impossible d\'ajouter la taxe !');
@@ -306,54 +396,77 @@ class FocusBack
 
   public function modifyTaxesPage()
   {
-    $taxesManager = new TaxesManager();
-    $modifyTax = $taxesManager->getTaxe($_GET['id']);
+      $taxesManager = new TaxesManager();
 
+      if (isset($_GET['id']) && $_GET['id'] > 0):
 
-    if ($modifyTax === false):
+          $modifyTax = $taxesManager->getTaxe($_GET['id']);
 
-        throw new Exception('Impossible d\'afficher le taxe page !');
+      else:
 
-    else:
+          throw new Exception("Aucun identifiant de taxe envoyé");
 
-        require('View/backend/modifyTax.php');
+      endif;
 
-    endif;
+      if ($modifyTax === false):
+
+          throw new Exception('Impossible d\'afficher le taxe page !');
+
+      else:
+
+          require('View/backend/modifyTax.php');
+
+      endif;
   }
 
   public function modifyTaxes()
   {
+      $taxesManager = new TaxesManager();
 
+      if (isset($_GET['id']) && $_GET['id'] > 0):
 
+          $updateTaxe = $taxesManager->updateTaxe($_GET['id'], $_POST['tax_date'], $_POST['tax_declare'], $_POST['tax_paid'], $_POST['tax_description']);
 
+      else:
 
+          throw new Exception("Aucun identifiant de taxe envoyé");
 
-    $taxesManager = new TaxesManager();
-    $updateTaxe = $taxesManager->updateTaxe($_GET['id'], $_POST['tax_date'], $_POST['tax_declare'], $_POST['tax_paid'], $_POST['tax_description']);
-    if ($updateTaxe === false):
+      endif;
 
-        throw new Exception('Impossible de modifier la taxe !');
+      if ($updateTaxe === false):
 
-    else:
+          throw new Exception('Impossible de modifier la taxe !');
 
-        header('Location: index.php?action=taxe&id='.$_GET['id'].'');
+      else:
 
-    endif;
+          header('Location: index.php?action=taxe&id='.$_GET['id'].'');
+
+      endif;
   }
 
   public function removeTaxe()
   {
-    $taxesManager = new TaxesManager();
-    $deleteTaxe = $taxesManager->deleteTaxe($_GET['id']);
-    if ($deleteTaxe === false):
+      $taxesManager = new TaxesManager();
 
-        throw new Exception('Impossible de supprimer la taxe !');
+      if (isset($_GET['id']) && $_GET['id'] > 0):
 
-    else:
+          $deleteTaxe = $taxesManager->deleteTaxe($_GET['id']);
 
-        header('Location: index.php?action=dashboard');
+      else:
 
-    endif;
+          throw new Exception("Aucun identifiant de taxe envoyé");
+
+      endif;
+
+      if ($deleteTaxe === false):
+
+          throw new Exception('Impossible de supprimer la taxe !');
+
+      else:
+
+          header('Location: index.php?action=dashboard');
+
+      endif;
   }
 
 }

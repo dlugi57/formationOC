d89f6bf46997fa47153206135da199430a8da17c
dlugi57 <dlugoszpeter@gmail.com>
2019-05-02 11:25:20 +0200

    SECURITY add exceptions into dashboard class

--- Projet5/focus/Controller/Dashboard.php
+++ Projet5/focus/Controller/Dashboard.php
@@ -14,161 +14,254 @@ use Exception;
 
 class Dashboard
 {
-  public function dashboard()
-  {
-    $clientsManager = new ClientManager();
-    $seancesManager = new SeanceManager();
-    $commandsManager = new CommandManager();
-    //GLOBAL CASH 4 mini widgets
-    $sumNet =  Dashboard::sumNet();
-    $sumBrut = Dashboard::sumBrut();
-    $countClients = $clientsManager->countClients();
-    $countSeances = $seancesManager->countSeances();
-
-    //CLIENTS
-    $contactBy = $clientsManager->contactBy();
-    $clientsList = $clientsManager->getClients();
-
-    //SEANCES
-    $countFutureSeances = $seancesManager->countFutureSeances();
-    $sumBrutSeances = $seancesManager->totals();
-    $typeSession = $seancesManager->typeSession();
-    $seancesList= $seancesManager->getFutureSeances();
-    //$seancesList= $seancesManager->getSeances();
-
-    //COMMANDS
-    $sumMonthCmd = $commandsManager->monthCmd();
-
-    //Monthly recaps send arrays into JS
-    $resultsMonthPaiedTax = Dashboard::monthTax();
-    $resultsNb = Dashboard::monthClients();
-    $resultsMonth = Dashboard::monthList();
-    $resultsNbSeance = Dashboard::monthBySeances();
-    $resultsMonthCashNet = Dashboard::monthNet();
-    $resultsMonthCash = Dashboard::monthBrut();
-
-    //facebook
-    $facebook = Dashboard::facebook();
-    require('View/frontend/dashboard.php');
-
-  }
-
-  public function monthBrut()
-  {
-    $seancesManager = new SeanceManager();
-    $monthSeancesCmd = $seancesManager->monthSeancesCmd();
-    $resultsMonthCash = array();
-    while ($data = $monthSeancesCmd->fetch())
+    public function dashboard()
     {
-      $entrance = htmlspecialchars($data['seance_cash']) + htmlspecialchars($data['cmd_cash']);
-      array_push($resultsMonthCash, intval($entrance));
-    }
-    return $resultsMonthCash;
-  }
+        $clientsManager = new ClientManager();
+        $seancesManager = new SeanceManager();
+        $commandsManager = new CommandManager();
+        //GLOBAL CASH 4 mini widgets
+        $sumNet =  Dashboard::sumNet();
+        $sumBrut = Dashboard::sumBrut();
+        $countClients = $clientsManager->countClients();
+        $countSeances = $seancesManager->countSeances();
 
-  public function monthNet()
-  {
-    $seancesManager = new SeanceManager();
-    $monthSeancesCmd = $seancesManager->monthSeancesCmd();
-    $resultsMonthCashNet = array();
-    while ($data = $monthSeancesCmd->fetch())
+        //CLIENTS
+        $contactBy = $clientsManager->contactBy();
+        $clientsList = $clientsManager->getClients();
+
+        //SEANCES
+        $countFutureSeances = $seancesManager->countFutureSeances();
+        $sumBrutSeances = $seancesManager->totals();
+        $typeSession = $seancesManager->typeSession();
+        $seancesList= $seancesManager->getFutureSeances();
+
+        //COMMANDS
+        $sumMonthCmd = $commandsManager->monthCmd();
+
+        //Monthly recaps send arrays into JS
+        $resultsMonthPaiedTax = Dashboard::monthTax();
+        $resultsNb = Dashboard::monthClients();
+        $resultsMonth = Dashboard::monthList();
+        $resultsNbSeance = Dashboard::monthBySeances();
+        $resultsMonthCashNet = Dashboard::monthNet();
+        $resultsMonthCash = Dashboard::monthBrut();
+
+        //facebook
+        $facebook = Dashboard::facebook();
+
+        if ($countClients === false || $countSeances === false):
+
+            throw new Exception('Impossible d\'afficher le widgets contenue !');
+
+        elseif($contactBy === false || $clientsList === false) :
+
+            throw new Exception('Impossible d\'afficher le client contenue !');
+
+        elseif($countFutureSeances === false || $sumBrutSeances === false || $typeSession === false || $seancesList === false) :
+
+            throw new Exception('Impossible d\'afficher le seance contenue !');
+
+        elseif($sumMonthCmd === false) :
+
+            throw new Exception('Impossible d\'afficher le command contenue !');
+
+        else:
+
+            require('View/frontend/dashboard.php');
+
+        endif;
+    }
+
+    public function monthBrut()
     {
-      $depenses = htmlspecialchars($data['seances_km']) * 0.15 + htmlspecialchars($data['cmd_km']) * 0.15 + htmlspecialchars($data['seance_depense']) + htmlspecialchars($data['cmd_depense']);
-      $entrance = htmlspecialchars($data['seance_cash']) + htmlspecialchars($data['cmd_cash']);
-      $cashNet = $entrance - $depenses;
-      array_push($resultsMonthCashNet,intval($cashNet));
-    }
-    return $resultsMonthCashNet;
-  }
+        $seancesManager = new SeanceManager();
+        $monthSeancesCmd = $seancesManager->monthSeancesCmd();
 
-  public function monthBySeances()
-  {
-    $seancesManager = new SeanceManager();
-    $monthSeances = $seancesManager->monthSeances();
-    $resultsNbSeance = array();
-    while ($data = $monthSeances->fetch())
+        if ($monthSeancesCmd === false):
+
+            throw new Exception('Impossible d\'afficher le month brut contenue !');
+
+        else:
+
+            $resultsMonthCash = array();
+            while ($data = $monthSeancesCmd->fetch())
+            {
+              $entrance = htmlspecialchars($data['seance_cash']) + htmlspecialchars($data['cmd_cash']);
+              array_push($resultsMonthCash, intval($entrance));
+            }
+
+            return $resultsMonthCash;
+
+        endif;
+    }
+
+    public function monthNet()
     {
-      array_push($resultsNbSeance,intval(htmlspecialchars($data['nb'])));
-    }
-    return $resultsNbSeance;
-  }
+        $seancesManager = new SeanceManager();
+        $monthSeancesCmd = $seancesManager->monthSeancesCmd();
 
-  public function monthList()
-  {
-    $clientsManager = new ClientManager();
-    $monthClients = $clientsManager->monthClients();
-    $resultsMonth = array();
-    while ($data = $monthClients->fetch())
+        if ($monthSeancesCmd === false):
+
+            throw new Exception('Impossible d\'afficher le month brut contenue!');
+
+        else:
+
+          $resultsMonthCashNet = array();
+          while ($data = $monthSeancesCmd->fetch())
+          {
+            $depenses = htmlspecialchars($data['seances_km']) * 0.15 + htmlspecialchars($data['cmd_km']) * 0.15 + htmlspecialchars($data['seance_depense']) + htmlspecialchars($data['cmd_depense']);
+            $entrance = htmlspecialchars($data['seance_cash']) + htmlspecialchars($data['cmd_cash']);
+            $cashNet = $entrance - $depenses;
+            array_push($resultsMonthCashNet,intval($cashNet));
+          }
+          return $resultsMonthCashNet;
+
+        endif;
+    }
+
+    public function monthBySeances()
     {
-      $monthNum  = htmlspecialchars($data['month']);
-      $monthName = date('F', mktime(0, 0, 0, $monthNum, 10));
-      array_push($resultsMonth, $monthName);
-    }
-    return $resultsMonth;
-  }
+        $seancesManager = new SeanceManager();
+        $monthSeances = $seancesManager->monthSeances();
 
-  public function monthClients()
-  {
-    $clientsManager = new ClientManager();
-    $monthClients = $clientsManager->monthClients();
-    $resultsNb = array();
-    while ($data = $monthClients->fetch())
+        if ($monthSeances === false):
+
+            throw new Exception('Impossible d\'afficher le monthBySeances contenue!');
+
+        else:
+
+            $resultsNbSeance = array();
+            while ($data = $monthSeances->fetch())
+            {
+              array_push($resultsNbSeance,intval(htmlspecialchars($data['nb'])));
+            }
+            return $resultsNbSeance;
+
+        endif;
+    }
+
+    public function monthList()
     {
-      array_push($resultsNb,intval(htmlspecialchars($data['nb'])));
-    }
-    return $resultsNb;
-  }
+        $clientsManager = new ClientManager();
+        $monthClients = $clientsManager->monthClients();
 
-  public function monthTax()
-  {
-    $taxesManager = new TaxesManager();
-    $monthPaiedTax = $taxesManager->monthPaiedTax();
-    $resultsMonthPaiedTax = array();
-    while ($data = $monthPaiedTax->fetch())
+        if ($monthClients === false):
+
+            throw new Exception('Impossible d\'afficher le monthList contenue!');
+
+        else:
+
+            $resultsMonth = array();
+            while ($data = $monthClients->fetch())
+            {
+              $monthNum  = htmlspecialchars($data['month']);
+              $monthName = date('F', mktime(0, 0, 0, $monthNum, 10));
+              array_push($resultsMonth, $monthName);
+            }
+            return $resultsMonth;
+
+        endif;
+    }
+
+    public function monthClients()
     {
-      $sumTax  = htmlspecialchars($data['taxesMonth']);
-      array_push($resultsMonthPaiedTax, intval($sumTax));
+        $clientsManager = new ClientManager();
+        $monthClients = $clientsManager->monthClients();
+
+        if ($monthClients === false):
+
+            throw new Exception('Impossible d\'afficher le monthClients contenue!');
+
+        else:
+
+            $resultsNb = array();
+            while ($data = $monthClients->fetch())
+            {
+              array_push($resultsNb,intval(htmlspecialchars($data['nb'])));
+            }
+            return $resultsNb;
+
+        endif;
     }
-    return $resultsMonthPaiedTax;
-  }
 
-  public function sumNet(){
-    $seancesManager = new SeanceManager();
-    $sumNetSeances = $seancesManager->totals();
-    $commandsManager = new CommandManager();
-    $sumNetCmd = $commandsManager->totalsCmd();
-    $taxesManager = new TaxesManager();
-    $sumTaxes = $taxesManager->totalsTax();
-    $summarySeances = htmlspecialchars($sumNetSeances['sumPrise']) - htmlspecialchars($sumNetSeances['sumDep']) - (htmlspecialchars($sumNetSeances['sumKm']) * 0.15);
-    $sumaryCmd = htmlspecialchars($sumNetCmd['sumPriseCmd']) - htmlspecialchars($sumNetCmd['sumDepCmd']);
-    $summary = $summarySeances + $sumaryCmd - htmlspecialchars($sumTaxes['sumPaidTax']);
+    public function monthTax()
+    {
+        $taxesManager = new TaxesManager();
+        $monthPaiedTax = $taxesManager->monthPaiedTax();
 
-    return $summary;
-  }
+        if ($monthPaiedTax === false):
 
-  public function sumBrut(){
-    $seancesManager = new SeanceManager();
-    $sumBrutSeances = $seancesManager->totals();
-    $commandsManager = new CommandManager();
-    $sumBrutCmd = $commandsManager->totalsCmd();
-    $summary = htmlspecialchars($sumBrutSeances['sumPrise']) + htmlspecialchars($sumBrutCmd['sumPriseCmd']);
+            throw new Exception('Impossible d\'afficher le monthTax contenue!');
 
-    return $summary;
-  }
+        else:
 
-  public function facebook()
-  {
-    $urlFB = "https://graph.facebook.com/";
-    $fb_page = 'SunnyMomentsPhoto';
-    $infosFB = '?fields=engagement,name,rating_count,overall_star_rating,username,cover,picture.height(300)&access_token=';
-    $access_token = 'EAAGkpQmbZCR8BAL4au06FW7vxBmNMOl1ybndBuITDs2szMaapszs0YT7SZCWs1zLHdy7KyCRRur2MAxDTZADrO1bhGyVu47hDXRue2ew8RRw1fwdZBvgm94PtZAunlwFmHOg4mu6kWan0TLKMrBCteRe361D6sE9VNkeWZCvXZA1244UyY0syiW';
-    $url = $urlFB.$fb_page.$infosFB.$access_token;
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($curl);
-    curl_close($curl);
-    $detailsFB = json_decode($result,true);
-    return $detailsFB;
-  }
+            $resultsMonthPaiedTax = array();
+            while ($data = $monthPaiedTax->fetch())
+            {
+              $sumTax  = htmlspecialchars($data['taxesMonth']);
+              array_push($resultsMonthPaiedTax, intval($sumTax));
+            }
+            return $resultsMonthPaiedTax;
+
+        endif;
+    }
+
+    public function sumNet()
+    {
+        $seancesManager = new SeanceManager();
+        $sumNetSeances = $seancesManager->totals();
+        $commandsManager = new CommandManager();
+        $sumNetCmd = $commandsManager->totalsCmd();
+        $taxesManager = new TaxesManager();
+        $sumTaxes = $taxesManager->totalsTax();
+
+        if ($sumNetSeances === false || $sumNetCmd === false || $sumTaxes === false):
+
+            throw new Exception('Impossible d\'afficher le sumNet contenue!');
+
+        else:
+
+            $summarySeances = htmlspecialchars($sumNetSeances['sumPrise']) - htmlspecialchars($sumNetSeances['sumDep']) - (htmlspecialchars($sumNetSeances['sumKm']) * 0.15);
+            $sumaryCmd = htmlspecialchars($sumNetCmd['sumPriseCmd']) - htmlspecialchars($sumNetCmd['sumDepCmd']);
+            $summary = $summarySeances + $sumaryCmd - htmlspecialchars($sumTaxes['sumPaidTax']);
+
+            return $summary;
+
+        endif;
+    }
+
+    public function sumBrut()
+    {
+        $seancesManager = new SeanceManager();
+        $sumBrutSeances = $seancesManager->totals();
+        $commandsManager = new CommandManager();
+        $sumBrutCmd = $commandsManager->totalsCmd();
+
+        if ($sumBrutSeances === false || $sumBrutCmd === false):
+
+            throw new Exception('Impossible d\'afficher le sumNet contenue!');
+
+        else:
+
+            $summary = htmlspecialchars($sumBrutSeances['sumPrise']) + htmlspecialchars($sumBrutCmd['sumPriseCmd']);
+            return $summary;
+
+        endif;
+    }
+
+    public function facebook()
+    {
+        $urlFB = "https://graph.facebook.com/";
+        $fb_page = 'SunnyMomentsPhoto';
+        $infosFB = '?fields=engagement,name,rating_count,overall_star_rating,username,cover,picture.height(300)&access_token=';
+        $access_token = 'EAAGkpQmbZCR8BAL4au06FW7vxBmNMOl1ybndBuITDs2szMaapszs0YT7SZCWs1zLHdy7KyCRRur2MAxDTZADrO1bhGyVu47hDXRue2ew8RRw1fwdZBvgm94PtZAunlwFmHOg4mu6kWan0TLKMrBCteRe361D6sE9VNkeWZCvXZA1244UyY0syiW';
+        $url = $urlFB.$fb_page.$infosFB.$access_token;
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        curl_close($curl);
+        $detailsFB = json_decode($result,true);
+        return $detailsFB;
+    }
 }

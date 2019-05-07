c96c2d6b51fe8597ec9b6969dd5c8764027f2654
dlugi57 <dlugoszpeter@gmail.com>
2019-05-07 23:41:08 +0200

    CLEAN insta fb

--- Projet5/focus/View/frontend/widgets/fb.php
+++ Projet5/focus/View/frontend/widgets/fb.php
@@ -1,42 +1,42 @@
 
-  <!-- Widget: user widget style 1 -->
-  <div class="box box-widget widget-user">
-    <!-- Add the bg color to the header using any of the bg-* classes -->
-    <div class="widget-user-header bg-black" style="background: url('<?= $facebook['cover']['source']; ?>') center center;">
-      <h3 class="widget-user-username"><?= $facebook['name']; ?></h3>
-      <h5 class="widget-user-desc"><?= $facebook['username']; ?></h5>
-    </div>
-    <div class="widget-user-image">
-      <img class="img-circle" src="<?= $facebook['picture']['data']['url']  ?>" alt="User Avatar">
-    </div>
-    <div class="box-footer">
-      <div class="row">
-        <div class="col-sm-4 border-right">
-          <div class="description-block">
-            <h5 class="description-header"><?= $facebook['rating_count']; ?></h5>
-            <span class="description-text">VOTES</span>
-          </div>
-          <!-- /.description-block -->
-        </div>
-        <!-- /.col -->
-        <div class="col-sm-4 border-right">
-          <div class="description-block">
-            <h5 class="description-header"><?= $facebook['engagement']['count'] ?></h5>
-            <span class="description-text">FOLLOWERS</span>
-          </div>
-          <!-- /.description-block -->
-        </div>
-        <!-- /.col -->
-        <div class="col-sm-4">
-          <div class="description-block">
-            <h5 class="description-header"><?= $facebook['overall_star_rating']; ?>/5</h5>
-            <span class="description-text">RATING</span>
-          </div>
-          <!-- /.description-block -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </div>
+<!-- Widget: user widget style 1 -->
+<div class="box box-widget widget-user">
+  <!-- Add the bg color to the header using any of the bg-* classes -->
+  <div class="widget-user-header bg-black" style="background: url('<?= $facebook['cover']['source']; ?>') center center;">
+    <h3 class="widget-user-username"><?= $facebook['name']; ?></h3>
+    <h5 class="widget-user-desc"><?= $facebook['username']; ?></h5>
   </div>
-  <!-- /.widget-user -->
+  <div class="widget-user-image">
+    <img class="img-circle" src="<?= $facebook['picture']['data']['url']  ?>" alt="User Avatar">
+  </div>
+  <div class="box-footer">
+    <div class="row">
+      <div class="col-sm-4 border-right">
+        <div class="description-block">
+          <h5 class="description-header"><?= $facebook['rating_count']; ?></h5>
+          <span class="description-text">VOTES</span>
+        </div>
+        <!-- /.description-block -->
+      </div>
+      <!-- /.col -->
+      <div class="col-sm-4 border-right">
+        <div class="description-block">
+          <h5 class="description-header"><?= $facebook['engagement']['count'] ?></h5>
+          <span class="description-text">FOLLOWERS</span>
+        </div>
+        <!-- /.description-block -->
+      </div>
+      <!-- /.col -->
+      <div class="col-sm-4">
+        <div class="description-block">
+          <h5 class="description-header"><?= $facebook['overall_star_rating']; ?>/5</h5>
+          <span class="description-text">RATING</span>
+        </div>
+        <!-- /.description-block -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+</div>
+<!-- /.widget-user -->
--- Projet5/focus/View/frontend/widgets/insta.php
+++ Projet5/focus/View/frontend/widgets/insta.php
@@ -1,42 +1,42 @@
 
-  <!-- Widget: user widget style 1 -->
-  <div class="box box-widget widget-user">
-    <!-- Add the bg color to the header using any of the bg-* classes -->
-        <div class="widget-user-header bg-black" style="background: url('Public/img/instagram.jpg') center center;">
-      <h3 class="widget-user-username" id="instaFullName"></h3>
-      <h5 class="widget-user-desc" id="instaUserName"></h5>
-    </div>
-    <div class="widget-user-image">
-      <img class="img-circle" id="instaProfile" src="" alt="User Avatar">
-    </div>
-    <div class="box-footer">
-      <div class="row">
-        <div class="col-sm-4 border-right">
-          <div class="description-block">
-            <h5 class="description-header" id="instaPosts"></h5>
-            <span class="description-text">POSTS</span>
-          </div>
-          <!-- /.description-block -->
-        </div>
-        <!-- /.col -->
-        <div class="col-sm-4 border-right">
-          <div class="description-block">
-            <h5 class="description-header" id="instaFollowers"></h5>
-            <span class="description-text">FOLLOWERS</span>
-          </div>
-          <!-- /.description-block -->
-        </div>
-        <!-- /.col -->
-        <div class="col-sm-4">
-          <div class="description-block">
-            <h5 class="description-header"id="instaFollows"></h5>
-            <span class="description-text">FOLLOWS</span>
-          </div>
-          <!-- /.description-block -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </div>
+<!-- Widget: user widget style 1 -->
+<div class="box box-widget widget-user">
+  <!-- Add the bg color to the header using any of the bg-* classes -->
+      <div class="widget-user-header bg-black" style="background: url('Public/img/instagram.jpg') center center;">
+    <h3 class="widget-user-username" id="instaFullName"></h3>
+    <h5 class="widget-user-desc" id="instaUserName"></h5>
   </div>
-  <!-- /.widget-user -->
+  <div class="widget-user-image">
+    <img class="img-circle" id="instaProfile" src="" alt="User Avatar">
+  </div>
+  <div class="box-footer">
+    <div class="row">
+      <div class="col-sm-4 border-right">
+        <div class="description-block">
+          <h5 class="description-header" id="instaPosts"></h5>
+          <span class="description-text">POSTS</span>
+        </div>
+        <!-- /.description-block -->
+      </div>
+      <!-- /.col -->
+      <div class="col-sm-4 border-right">
+        <div class="description-block">
+          <h5 class="description-header" id="instaFollowers"></h5>
+          <span class="description-text">FOLLOWERS</span>
+        </div>
+        <!-- /.description-block -->
+      </div>
+      <!-- /.col -->
+      <div class="col-sm-4">
+        <div class="description-block">
+          <h5 class="description-header"id="instaFollows"></h5>
+          <span class="description-text">FOLLOWS</span>
+        </div>
+        <!-- /.description-block -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+</div>
+<!-- /.widget-user -->

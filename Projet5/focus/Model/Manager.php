ca788a12a0cc63eb69bfbd43f109dcd307f8e060
dlugi57 <dlugoszpeter@gmail.com>
2019-05-10 15:58:54 +0200

    CLEAN

--- Projet5/focus/Model/Manager.php
+++ Projet5/focus/Model/Manager.php
@@ -7,8 +7,7 @@ class Manager
     protected function dbConnect()
     {
         $db = new \PDO('mysql:host=localhost;dbname=focus;charset=utf8', 'root', '');
-
-        //$db = new \PDO('mysql:host=localhost;dbname=lingsoft_crm_piotr;charset=utf8', 'lingsoft_wpiotr', 'Spt!T]94j6');
+        
         return $db;
     }
 }
